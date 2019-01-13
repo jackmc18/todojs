df26a128288fd139f13a389b78cf4dcf897e4972
Jackmc <jackmc1122@gmail.com>
2019-01-12 20:30:18 -0500

    Show App on main page.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import SignIn from "../components/SignIn/SignIn";
 import "./App.css";
-import { setSignInEmailField } from "../actions";
-
-const mapStateToProps = state => {
-  return {
-    signInEmailField: state.signIn.signInEmailField
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onSignInEmailChange: event =>
-      dispatch(setSignInEmailField(event.target.value))
-  };
-};
 
 class App extends Component {
   render() {
-    const { onSignInEmailChange } = this.props;
-    return (
-      <div>
-        <SignIn signInEmailChange={onSignInEmailChange} />
-      </div>
-    );
+    return <div>App</div>;
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default App;
