2b75c93bd87a6c98729bd89304646acac9690dcc
Jackmc <jackmc1122@gmail.com>
2019-01-15 13:38:13 -0500

    Add Card and CardList component and connect CardList with Board using default names.

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,7 +1,27 @@
 import React from "react";
+import CardList from "../CardList/CardList";
 
-const Board = () => {
-  return <div>Board</div>;
+const initialState = {
+  boardName: "Untitled",
+  boardOwner: null,
+  cardLists: ["To Do", "Doing", "Done"]
 };
+
+class Board extends React.Component {
+  state = initialState;
+
+  render() {
+    const cardLists = this.state.cardLists.map(cardList => {
+      return <CardList name={cardList} />;
+    });
+
+    return (
+      <div>
+        <h3>Board</h3>
+        {cardLists}
+      </div>
+    );
+  }
+}
 
 export default Board;
