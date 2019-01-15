1b399274a22289cab4d03903622d4db51001b06c
Jackmc <jackmc1122@gmail.com>
2019-01-14 23:10:45 -0500

    Reimplement the top nav with pure css horizontal menu.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,22 +3,63 @@ import { Link } from "react-router-dom";
 import "./Navigation.css";
 
 const Navigation = ({ isSignedIn, signOut }) => {
-  if (isSignedIn) {
+  if (isSignedIn)
     return (
-      <nav>
-        <Link to="/signin" onClick={signOut}>
-          Sign Out
-        </Link>
-      </nav>
+      <div className="pure-menu pure-menu-horizontal">
+        <a href="#" className="pure-menu-heading pure-menu-link">
+          ORGANIZER
+        </a>
+        <ul className="pure-menu-list right">
+          <li className="pure-menu-item">
+            <Link to="/boardList" className="pure-menu-link">
+              Boards
+            </Link>
+          </li>
+          <li className="pure-menu-item">
+            <Link to="/signin" className="pure-menu-link">
+              Sign Out
+            </Link>
+          </li>
+        </ul>
+      </div>
     );
-  } else {
-    return (
-      <nav>
-        <Link to="/signin">SignIn</Link>
-        <Link to="/register">Register</Link>
-      </nav>
-    );
-  }
+
+  return (
+    <div className="pure-menu pure-menu-horizontal">
+      <a href="#" className="pure-menu-heading pure-menu-link">
+        ORGANIZER
+      </a>
+      <ul className="pure-menu-list right">
+        <li className="pure-menu-item">
+          <Link to="/register" className="pure-menu-link">
+            Register
+          </Link>
+        </li>
+        <li className="pure-menu-item">
+          <Link to="/signin" className="pure-menu-link">
+            Sign In
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+
+  // if (isSignedIn) {
+  //   return (
+  //     <nav>
+  //       <Link to="/signin" onClick={signOut}>
+  //         Sign Out
+  //       </Link>
+  //     </nav>
+  //   );
+  // } else {
+  //   return (
+  //     <nav>
+  //       <Link to="/signin">SignIn</Link>
+  //       <Link to="/register">Register</Link>
+  //     </nav>
+  //   );
+  // }
 };
 
 export default Navigation;
