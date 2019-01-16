b651209ae13c846150929862cf8425f2a1ce3230
Jackmc <jackmc1122@gmail.com>
2019-01-16 17:06:07 -0500

    Create Board now takes the user to a specific url for the board.

--- src/components/BoardList/BoardList.js
+++ src/components/BoardList/BoardList.js
@@ -2,26 +2,58 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const initialState = {
-  boards: []
+  boards: [],
+  createBoardFlag: false,
+  createBoardName: ""
 };
 
 class BoardList extends React.Component {
   state = initialState;
 
-  onCreateBoard = () => {
+  onCreateBoardToggle = () => {
     this.setState({ createBoardFlag: true });
+  };
+
+  onCreateBoardConfirm = () => {
+    this.setState({
+      createBoardToggle: false,
+      boards: [...this.state.boards, this.state.createBoardName]
+    });
+  };
+
+  onCreateBoardNameChange = event => {
+    this.setState({ createBoardName: event.target.value });
   };
 
   render() {
     return (
       <div>
         <h3>Board List</h3>
-        <Link
-          to={{ pathname: "/board/default" }}
-          className="pure-button pure-button-primary"
-        >
-          CreateBoard
-        </Link>
+        {this.state.createBoardFlag ? (
+          <div className="create-board">
+            <input
+              className="create-board"
+              placeholder="Board Name"
+              onChange={this.onCreateBoardNameChange}
+            />
+            <Link
+              onClick={this.onCreateBoardConfirm}
+              to={{ pathname: `/board/${this.state.createBoardName}` }}
+              className="pure-button pure-button-primary"
+            >
+              CreateBoard
+            </Link>
+          </div>
+        ) : (
+          <div className="create-board">
+            <button
+              onClick={this.onCreateBoardToggle}
+              className="pure-button pure-button-primary"
+            >
+              CreateBoard
+            </button>
+          </div>
+        )}
       </div>
     );
   }
