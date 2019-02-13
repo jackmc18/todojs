f2e52d1ec05f3e0aebde879355caf40cd6416d00
Jackmc <jackmc1122@gmail.com>
2019-02-13 15:09:50 -0500

    Implemetn the register component with material ui.

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,5 +1,48 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
+import PropTypes from "prop-types";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+const styles = theme => ({
+  main: {
+    width: "auto",
+    display: "block", // Fix IE 11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+      .spacing.unit * 3}px`
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing.unit
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3
+  }
+});
 
 class Register extends React.Component {
   constructor(props) {
@@ -24,7 +67,8 @@ class Register extends React.Component {
     this.setState({ password: event.target.value });
   };
 
-  onSubmitRegister = () => {
+  onSubmitRegister = event => {
+    event.preventDefault();
     fetch("http://localhost:3000/register", {
       method: "post",
       headers: { "Content-Type": "application/json" },
@@ -45,58 +89,70 @@ class Register extends React.Component {
 
   render() {
     const { isSignedIn } = this.state;
+    const { classes } = this.props;
 
     if (isSignedIn) {
       return <Redirect to="/boardList" />;
     }
 
     return (
-      <article className="center">
-        <main>
-          <div>
-            <form className="">
-              <fieldset>
-                <legend>Register</legend>
-                <label htmlFor="name">Name</label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  placeholder="Name"
-                  onChange={this.onNameChange}
-                />
-
-                <label htmlFor="email">Email</label>
-                <input
-                  type="email"
-                  name="email-address"
-                  id="email-address"
-                  placeholder="Email"
-                  onChange={this.onEmailChange}
-                />
-
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="Password"
-                  onChange={this.onPasswordChange}
-                />
-
-                <input
-                  type="button"
-                  value="Register"
-                  className=""
-                  onClick={this.onSubmitRegister}
-                />
-              </fieldset>
-            </form>
-          </div>
-        </main>
-      </article>
+      <main className={classes.main}>
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Register
+          </Typography>
+          <form className={classes.form}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="name">Name</InputLabel>
+              <Input
+                id="name"
+                name="name"
+                autoComplete="name"
+                onChange={this.onNameChange}
+                autoFocus
+              />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="email">Email Address</InputLabel>
+              <Input
+                id="email"
+                name="email"
+                autoComplete="email"
+                onChange={this.onEmailChange}
+              />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={this.onPasswordChange}
+              />
+            </FormControl>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={this.onSubmitRegister}
+            >
+              Register
+            </Button>
+          </form>
+        </Paper>
+      </main>
     );
   }
 }
 
-export default Register;
+Register.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Register);
