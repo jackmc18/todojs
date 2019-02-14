faca23ebcdfac58496cc89f050d12b9212d3b954
Jackmc <jackmc1122@gmail.com>
2019-02-14 17:36:51 -0500

    Finish basic menu with signin/signout functionality and home/boards menu.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,6 +8,12 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import MenuList from "@material-ui/core/MenuList";
+import MenuItem from "@material-ui/core/MenuItem";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
 
 const styles = {
   nav: {
@@ -22,43 +28,112 @@ const styles = {
   }
 };
 
-const Navigation = ({ isSignedIn, signOut, classes, history }) => {
-  const onSignOut = event => {
+const initialState = {
+  menuOpen: false
+};
+
+class Navigation extends React.Component {
+  state = initialState;
+
+  onSignOut = event => {
     event.preventDefault();
-    signOut();
-    history.push("/signin");
+    this.props.signOut();
+    this.props.history.push("/signin");
   };
 
-  return (
-    <div className={classes.nav}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="Menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
-            News
-          </Typography>
-          {isSignedIn !== null ? (
-            isSignedIn ? (
-              <Button onClick={onSignOut} color="inherit">
-                Sign Out
-              </Button>
-            ) : (
-              <Button onClick={onSignOut} color="inherit">
-                Sign In
-              </Button>
-            )
-          ) : null}
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-};
+  handleMenuToggle = () => {
+    this.setState(state => ({ menuOpen: !state.open }));
+  };
+
+  handleMenuClose = event => {
+    if (this.anchorEl.contains(event.target)) {
+      return;
+    }
+
+    this.setState({ menuOpen: false });
+  };
+
+  handleMenuSelectHome = event => {
+    this.handleMenuClose(event);
+    this.props.history.push("/");
+  };
+
+  handleMenuSelectBoards = event => {
+    this.handleMenuClose(event);
+    this.props.history.push("/boardlist");
+  };
+
+  render() {
+    const { isSignedIn, classes } = this.props;
+    const { menuOpen } = this.state;
+
+    return (
+      <div className={classes.nav}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+              buttonRef={node => {
+                this.anchorEl = node;
+              }}
+              aria-owns={menuOpen ? "menu-list-grow" : undefined}
+              aria-haspopup="true"
+              onClick={this.handleMenuToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Popper
+              open={menuOpen}
+              anchorEl={this.anchorEl}
+              transition
+              disablePortal
+            >
+              {({ TransitionProps, placement }) => (
+                <Grow
+                  {...TransitionProps}
+                  id="menu-list-grow"
+                  style={{
+                    transformOrigin:
+                      placement === "bottom" ? "center top" : "center bottom"
+                  }}
+                >
+                  <Paper>
+                    <ClickAwayListener onClickAway={this.handleMenuClose}>
+                      <MenuList>
+                        <MenuItem onClick={this.handleMenuSelectHome}>
+                          Home
+                        </MenuItem>
+                        <MenuItem onClick={this.handleMenuSelectBoards}>
+                          Boards
+                        </MenuItem>
+                      </MenuList>
+                    </ClickAwayListener>
+                  </Paper>
+                </Grow>
+              )}
+            </Popper>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              TODOJS
+            </Typography>
+            {isSignedIn !== null ? (
+              isSignedIn ? (
+                <Button onClick={this.onSignOut} color="inherit">
+                  Sign Out
+                </Button>
+              ) : (
+                <Button onClick={this.onSignOut} color="inherit">
+                  Sign In
+                </Button>
+              )
+            ) : null}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
 
 Navigation.propTypes = {
   classes: PropTypes.object.isRequired
