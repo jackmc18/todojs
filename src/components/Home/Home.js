0e28e94c542602d60f88cd2a01923cc7b68aaf89
Jackmc <jackmc1122@gmail.com>
2019-02-28 16:46:20 -0500

    Change Home to typography.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Typography component="h2" variant="h6">
+      Home
+    </Typography>
+  );
 };
 
 export default Home;
